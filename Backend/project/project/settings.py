abfd24efa37ba2cc66cf55bfb19b9860e5160731
Muhammed Jaseel C A <mhdjaseel663@gmail.com>
2025-08-24 12:30:07 +0530

    MySQL db connected to backend

--- Backend/project/project/settings.py
+++ Backend/project/project/settings.py
@@ -79,8 +79,14 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eclinic',
+        'USER':'root',
+        'PORT':3306,
+        'HOST':'localhost',
+        'PASSWORD':'mhdjaseel'
+
+
     }
 }
 
---
+++ Frontend/eclinic/src/Component/Patient/History/DoctorsList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function DoctorsList() {
+  return (
+    <div>
+        <div className="container">
+            <div className="card">
+                <div className="card-body">
+                    <h2>Doctor</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default DoctorsList
